ead4d549da2b39a7d546bec69fc53393268d0176
Fariz Luqman <fariz@studionexus.co>
2016-06-27 10:26:49 +0800

    Add PHP Unit Testing for App

--- app/Core/tests/AppTest.php
+++ app/Core/tests/AppTest.php
@@ -10,6 +10,16 @@ class AppTest extends TestCase
     public function testProducerFirst()
     {
 		$app = new Core\App;
+		$app->link('database', Core\Database::connect());
+		$app->link('cachemanager', Core\Cache::init());
+		$app->link('success', true);
+		Core\Sharer::share('app', $app);
+		$test = Core\Sharer::get();
+		if($test['app']->success == true){
+			$this->assertTrue(true);
+		}else{
+			$this->assertTrue(false);
+		}
 		$this->assertTrue(true);
         return 'first';
     }
