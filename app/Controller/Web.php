793eabdce35931feabd3fe04b2e6630ec461cf4c
farizluqman <fariz.fnb@gmail.com>
2017-10-04 19:19:32 +0800

    Release candidate 0.5

---
+++ app/Controller/Web.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * StupidlySimple - A PHP Framework For Lazy Developers
+ *
+ * @package		StupidlySimple
+ * @author		Fariz Luqman <fariz.fnb@gmail.com>
+ * @copyright	2017 Fariz Luqman
+ * @license		MIT
+ * @link		https://stupidlysimple.github.io/
+ */
+namespace Controller;
+
+/**
+ * Class Web
+ * @package Controller
+ */
+class Web {
+    public function start(){
+        // do nothing
+    }
+}
