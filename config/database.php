f511a2cee932a1555ed7927c1679dd70164712ea
farizluqman <fariz.fnb@gmail.com>
2017-10-06 02:19:16 +0800

    Fix database.php

--- config/database.php
+++ config/database.php
@@ -2,7 +2,6 @@
 /**
  * StupidlySimple - A PHP Framework For Lazy Developers.
  *
-<<<<<<< Updated upstream
  * @author		Fariz Luqman <fariz.fnb@gmail.com>
  * @copyright	2017 Fariz Luqman
  * @license		MIT
@@ -23,26 +22,3 @@ return
     'prefix'    => '',
   ],
 ];
-=======
- * @package        StupidlySimple
- * @author        Fariz Luqman <fariz.fnb@gmail.com>
- * @copyright    2017 Fariz Luqman
- * @license        MIT
- * @link        https://stupidlysimple.github.io/
- */
-return
-    [
-        'enabled' => true,
-        'settings' =>
-            [
-                'host' => 'localhost',
-                'driver' => 'mysql',
-                'database' => 'stupidlysimple',
-                'username' => 'root',
-                'password' => 'root',
-                'charset' => 'utf8',
-                'collation' => 'utf8_unicode_ci',
-                'prefix' => '',
-            ]
-    ];
->>>>>>> Stashed changes
