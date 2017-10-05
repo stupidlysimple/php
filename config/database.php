7ea059b0484081542c9d7e02c18e1bc4e9ad819b
farizluqman <fariz.fnb@gmail.com>
2017-10-06 02:16:48 +0800

    Edit database.php and .htaccess

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 /**
  * StupidlySimple - A PHP Framework For Lazy Developers.
  *
+<<<<<<< Updated upstream
  * @author		Fariz Luqman <fariz.fnb@gmail.com>
  * @copyright	2017 Fariz Luqman
  * @license		MIT
@@ -22,3 +23,26 @@ return
     'prefix'    => '',
   ],
 ];
+=======
+ * @package        StupidlySimple
+ * @author        Fariz Luqman <fariz.fnb@gmail.com>
+ * @copyright    2017 Fariz Luqman
+ * @license        MIT
+ * @link        https://stupidlysimple.github.io/
+ */
+return
+    [
+        'enabled' => true,
+        'settings' =>
+            [
+                'host' => 'localhost',
+                'driver' => 'mysql',
+                'database' => 'stupidlysimple',
+                'username' => 'root',
+                'password' => 'root',
+                'charset' => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix' => '',
+            ]
+    ];
+>>>>>>> Stashed changes
